d38aac2501ee6cf71a3e89a9f7fcd410a95a0898
Hyorry <Borieya0619@gmail.com>
2024-04-24 16:46:49 +0900

    first commit

---
+++ src/main/java/com/study/mcdonaldskiosk/McdonaldsKioskApplication.java
@@ -0,0 +1,13 @@
+package com.study.mcdonaldskiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McdonaldsKioskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McdonaldsKioskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/mcdonaldskiosk/McdonaldsKioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.mcdonaldskiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McdonaldsKioskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
