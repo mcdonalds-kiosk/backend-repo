14ef2cff08561dc6a4578cc6beeb5847dfa7194a
EunSo0 <dmstn423@naver.com>
2024-04-30 14:01:23 +0900

    feat: 토스페이먼츠 연동 완료

--- src/main/java/com/study/mcdonaldskiosk/dto/PurchaseDto.java
+++ src/main/java/com/study/mcdonaldskiosk/dto/PurchaseDto.java
@@ -2,6 +2,12 @@ package com.study.mcdonaldskiosk.dto;
 
 import com.study.mcdonaldskiosk.entity.Purchase;
 import lombok.*;
+<<<<<<< HEAD
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+=======
+>>>>>>> 738850bfbb64d6f99cd0ff80501465dcc6346172
 
 @Getter
 @Setter
@@ -9,6 +15,21 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PurchaseDto {
+<<<<<<< HEAD
+  private int memberIdx;
+  private String payType;
+  private Long amount;
+
+  public Purchase toEntity() {
+    return Purchase.builder()
+        .memberIdx(memberIdx)
+        .payType(payType)
+        .price(amount)
+        .amount(amount)
+        .status(1)
+        .createdAt(LocalDateTime.now())
+        .updatedAt(LocalDateTime.now())
+=======
   private String payType;
   private Long amount;
   private String orderId;
@@ -20,6 +41,7 @@ public class PurchaseDto {
         .payType(payType)
         .amount(amount)
         .status(0)
+>>>>>>> 738850bfbb64d6f99cd0ff80501465dcc6346172
         .build();
   }
 }
--- src/main/java/com/study/mcdonaldskiosk/entity/Purchase.java
+++ src/main/java/com/study/mcdonaldskiosk/entity/Purchase.java
@@ -1,9 +1,14 @@
 package com.study.mcdonaldskiosk.entity;
 
+<<<<<<< HEAD
+import jakarta.persistence.*;
+import lombok.*;
+=======
 import com.study.mcdonaldskiosk.dto.PurchaseResDto;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.Type;
+>>>>>>> 738850bfbb64d6f99cd0ff80501465dcc6346172
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -41,6 +46,8 @@ public class Purchase {
   @Column(name="updated_at", nullable = false)
   private LocalDateTime updatedAt = LocalDateTime.now();
 
+<<<<<<< HEAD
+=======
   public PurchaseResDto toPurchaseResDto() {
     return PurchaseResDto.builder()
         .payType(payType)
@@ -48,4 +55,5 @@ public class Purchase {
         .createdAt(String.valueOf(getCreatedAt()))
         .build();
   }
+>>>>>>> 738850bfbb64d6f99cd0ff80501465dcc6346172
 }
